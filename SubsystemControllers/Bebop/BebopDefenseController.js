08a14bb857192578644bbbf7fa820f4f96e870ce
Zac Waite <129032699zw@gmail.com>
2021-10-14 21:35:37 -0400

    Updated how to get subsystem data again

--- SubsystemControllers/Bebop/BebopDefenseController.js
+++ SubsystemControllers/Bebop/BebopDefenseController.js
@@ -1,13 +1,14 @@
 //Based on BebopDefenceController.cs
+import Vector2 from "../../Sandbox/Scripts/Helpers/Vector2";
 import DefenceController from "../../Sandbox/Scripts/AbstractSubsystemControllers/AbstractDefenseController"
 export default class BebopDefenceController extends DefenceController{
 	constructor(...args){
     	super(...args);
   	}
 	/* To get ship information, use the following functions:
-	  this.getSensors()
-	  this.getNavigation()
-	  this.getPropulsion()
+	  this.navigation
+	  this.propulsion
+	  this.sensors
 	  see SandBox/Scripts/Ship/README.md for an explanation of return values.
 	*/
   	DefenceUpdate(shipStatusInfo, turretControls, deltaTime){
--- SubsystemControllers/Bebop/BebopNavigationController.js
+++ SubsystemControllers/Bebop/BebopNavigationController.js
@@ -1,13 +1,14 @@
 //Based on BebopNavigationController.cs
+import Vector2 from "../../Sandbox/Scripts/Helpers/Vector2";
 import NavigationController from "../../Sandbox/Scripts/AbstractSubsystemControllers/AbstractNavigationController"
 export default class BebopNavigationController extends NavigationController{
 	constructor(...args){
     	super(...args);
   	}
 	/* To get ship information, use the following functions:
-	  this.getDefence()
-	  this.getSensors()
-	  this.getPropulsion()
+	  this.defence
+	  this.propulsion
+	  this.sensors
 	  see SandBox/Scripts/Ship/README.md for an explanation of return values.
 	*/
 	NavigationController(shipStatusInfo, galaxyMapData, deltaTime){
