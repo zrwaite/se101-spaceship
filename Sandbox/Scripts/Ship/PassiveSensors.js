44ca525c3ac26a5bef60a5d1a39ea82405cd980a
Zac Waite <129032699zw@gmail.com>
2021-11-07 15:20:16 -0500

    Sensors and ship info

--- Sandbox/Scripts/Ship/PassiveSensors.js
+++ Sandbox/Scripts/Ship/PassiveSensors.js
@@ -2,12 +2,12 @@
 
 import Vector2 from "../Helpers/Vector2";
 
-export default class PassiveSensors extends IPassiveSensors{
+export default class PassiveSensors{
     constructor(...args){
         super(...args);
-        this.GConstant = 1;
-        this.PassiveReadings = []; //PassiveSensorReading
-        this.InterferenceScale = 0; //Don't know what this does 
+        this.gConstant = 1;
+        this.passiveReadings = []; //PassiveSensorReading
+        this.interferenceScale = 0; //Don't know what this does 
         this.debugFont; //font file... this might have to be in css
     }
     _Ready(){
@@ -15,71 +15,33 @@ export default class PassiveSensors extends IPassiveSensors{
     }
 
     GeneratePassiveSensorReadings(){ // Long range sensor detection of WarpGates and LargeBodies
-        this.PassiveReadings = [];
+        this.passiveReadings = [];
         let overlaps = GetOverlappingAreas(); //I believe this function is built into godot, we will need to remake or change
-        for (let i = 0; i<=overlaps.length; i++){
-            let area = overlaps[i]
+        overlaps.forEach((area)=>{
             let ID = area.GetInstanceId(); //this function is from godot i think, will need to be remade/change
             switch (area){
                 case warpGate:
-                    randomInsideCircle = new Vector2(Math.random()*2-1, Math.random()*2-1).magnitude;
-                    
-            }
-        }
-
-        foreach (Area2D area in overlaps)
-        {
-            ulong ID = area.GetInstanceId();
-
-            switch (area)
-            {
-                case WarpGate warpGate:
-                    {
-                        Vector2 randomInsideCircle = new Vector2(random.RandfRange(-1f, 1f), random.RandfRange(-1f, 1f)).Normalized();
-                        Vector2 noiseOffset = randomInsideCircle * InterferenceScale;
-                        Vector2 positionDiff = (warpGate.GlobalPosition + noiseOffset) - GlobalPosition;
-                        //float angle = GetAngleTo(warpGate.GlobalPosition + noiseOffset);
-                        float angle = Vector2.Right.AngleTo(positionDiff);
-
-
-                        float waveAmplitude = GConstant / positionDiff.Length();
-
-                        PassiveReadings.Add(new PassiveSensorReading(ID, angle, /*waveAmplitude,*/ GravitySignature.WarpGate/*, warpGate.DestinationSolarSystemName*/));
-                    }
+                    let randomInsideCircle = new Vector2(Math.random()*2-1, Math.random()*2-1).magnitude;
+                    let noiseOffset = randomInsideCircle * this.InterferenceScale;
+                    let positionDiff = (warpGate.GlobalPosition.add(noiseOffset)).subtract(GlobalPosition);
+                    let angle = Vector2.right.angleTo(positionDiff);
+                    this.passiveReadings.push(new passiveSensorReading(ID, angle, gravitySignature.warpGate));
                     break;
-
-                case LargeBody largeBody:
-                    {
-                        Vector2 randomInsideCircle = new Vector2(random.RandfRange(-1f, 1f), random.RandfRange(-1f, 1f)).Normalized();
-                        Vector2 noiseOffset = randomInsideCircle * InterferenceScale;
-                        Vector2 noisyVectorToBody = (largeBody.GlobalPosition + noiseOffset) - GlobalPosition;
-
-                        float angle = Vector2.Right.AngleTo(noisyVectorToBody);
-                        float waveAmplitude = GConstant / noisyVectorToBody.Length();
-                        
-                        GravitySignature signature = largeBody.GravitySignature;
-
-                        PassiveReadings.Add(new PassiveSensorReading(ID, angle, /*waveAmplitude,*/ signature));
-                    }
+                case largeBody:
+                    let randomInsideCircle = new Vector2(Math.random()*2-1, Math.random()*2-1).magnitude;
+                    let noiseOffset = randomInsideCircle * this.InterferenceScale;
+                    let positionDiff = (largeBody.GlobalPosition.add(noiseOffset)).subtract(GlobalPosition);
+                    let angle = Vector2.right.angleTo(positionDiff);
+                    let signature = largeBody.GravitySignature;
+                    this.passiveReadings.push(new passiveSensorReading(ID, angle, signature));
                     break;
             }
-        }
+        });
     }
-
-    public override void _Process(float delta)
-    {
+    _Process (delta){
         Update();
     }
+    _Draw(){
 
-    public override void _Draw()
-    {
-        // DrawSetTransformMatrix(GetGlobalTransform().Inverse());
-
-        // foreach (var reading in GWIReadings)
-        // {
-        //     DrawLine(GlobalPosition, GlobalPosition + (Vector2.Right * 1f / reading.Amplitude).Rotated(reading.Angle), Colors.Yellow);
-        // }
-
-        // DrawSetTransformMatrix(GetGlobalTransform());
     }
 }
---
+++ Sandbox/Scripts/Ship/SpaceMaterials.js
@@ -0,0 +1,12 @@
+//Based on GravitySignature.cs
+const SpaceMaterials = {
+    None: 0,
+    Unknown: 1,
+    Common: 2,
+    Metal: 3,
+    Water: 4,
+    Fisable: 5,
+    Antimatter: 6   
+};
+
+export default SpaceMaterials;
