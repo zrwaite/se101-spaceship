a1411fd1f9e77fccf3e7b5325a51e2017c8194f9
Zac Waite <129032699zw@gmail.com>
2021-11-07 15:20:07 -0500

    Thrusters and propulsion

---
+++ Sandbox/Scripts/Ship/ShipPropulsionMode.js
@@ -0,0 +1,8 @@
+//Based on GravitySignature.cs
+const ShipPropulsionMode = {
+    AutomaticController: 0,
+    ManualThrusterDrive: 1,
+    ManualUFODrive: 2
+};
+
+export default ShipPropulsionMode;
