7363bd4f4b7634a8703ce66b02dc60801ecb88e7
Zac Waite <129032699zw@gmail.com>
2021-11-07 22:53:19 -0500

    Added level design classes

---
+++ src/solarSystem.js
@@ -0,0 +1,32 @@
+import Vector2 from "../Sandbox/"
+import Asteroid from "./spaceObjects/asteroid.js";
+import WarpGate from "./spaceObjects/warpGate.js";
+import Planet from "./spaceObjects/planet.js";
+import AsteroidLauncher from "./spaceObjects/asteroidLauncher.js";
+
+//Parent class for solarsystems, use data from solarsystem jsons and galaxy and spaceobjects scripts
+//The constructor is going to build the levels
+//Think of this as sort of a level builder
+export default class SolarSystem{
+	constructor(warpGates){
+		this.asteroids = [];
+		this.warpGates = [];
+		this.planets = [];
+		this.asteroidLaunchers = [];
+		this.startingSolarSystem;
+		warpGates.forEach((warpGate)=>{this.addWarpGate(warpGate);})
+		switch(galaxyName){
+			case "Alpha Centauri":
+				// asteroid1 = new Asteroid(new Vector2(1,2));
+				// planet1 = new Planet(new Vector2(30,40));
+				// asteroidLauncher1 = new AsteroidLauncher(new Vector2(100,200));
+				// create asteroids, planets and launchers. Not warp gates
+				break;
+			case "Aquarii": 
+				break;
+		}
+	}
+	addWarpGate(){
+		//Link togethers solarsystems with warpgates
+	}
+}
