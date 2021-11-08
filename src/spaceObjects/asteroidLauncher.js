fe70a3256d19f12a3c738adaec3108d6b2c86837
Zac Waite <129032699zw@gmail.com>
2021-11-07 22:52:25 -0500

    Added spaceobject classes

---
+++ src/spaceObjects/asteroidLauncher.js
@@ -0,0 +1,10 @@
+import Asteroid from "./asteroid.js";
+class AsteroidLauncher{
+	constructor(pos, game) {
+		this.game = game;
+		this.pos = pos;
+	}
+	launchAsteroid(){
+		//create function to create new asteroid and yeet it
+	}
+}	
---
+++ src/spaceObjects/warpGate.js
@@ -0,0 +1,13 @@
+import Sprite from "../sprite.js";
+export default class WarpGate extends RenderedObject {
+	constructor(...args) {
+		super(...args);
+		this.image = document.getElementById("asteroid");
+		this.height = 50;
+		this.width = 50;
+	}
+	update(deltaTime) {
+		//Add special update code here if needed
+		super.update();
+	}
+}
