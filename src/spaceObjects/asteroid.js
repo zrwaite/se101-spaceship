b77684e36d6ce6c97c12c49b90654cc9c6212647
Zac Waite <129032699zw@gmail.com>
2021-11-15 16:10:55 -0500

    Boundary control adjustments

--- src/spaceObjects/asteroid.js
+++ src/spaceObjects/asteroid.js
@@ -14,12 +14,17 @@ export default class Asteroid extends Sprite {
 	update() {
 		//Add special update code here if needed
 		super.update();
-
+		this.boundaries();
 	}
 	shatter() {
 		//Create a bunch of meteors, somewhat randomly.
 		//this.delete = true
 		// meteor1 = new Meteor(new Vector2(50, 30), this.pos, this.game);
 		// this.game.delObjects.push(meteor1);
+	}
+	boundaries(){
+		if (this.pos.y>this.game.height+this.size.y || this.pos.y<-this.size.y || 
+			this.pos.x>this.game.width+this.size.x || this.pos.x<-this.size.x) 
+			this.delete=true; // bounds detection for deletion
 	}
 }
