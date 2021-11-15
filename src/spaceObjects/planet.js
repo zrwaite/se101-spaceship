ca727b02ad43e72ee2c5a29f3bb40a9335db75ac
Zac Waite <129032699zw@gmail.com>
2021-11-15 16:10:35 -0500

    Adjusting planet rendering

--- src/spaceObjects/planet.js
+++ src/spaceObjects/planet.js
@@ -2,14 +2,14 @@ import RenderedObject from "../renderedObject.js";
 import Vector2 from "../helpers/Vector2.js";
 
 export default class Planet extends RenderedObject {
-	constructor(...args) {
+	constructor(planetName, ...args) {
 		super(...args);
-		this.image = document.getElementById("default-planet");
-		this.height = 150;
-		this.width = 150;
+		this.ctx = "background";
+		this.image = this.game.images[planetName];
+		this.size = new Vector2(35, 35);
 	}
 	update() {
 		//Add special update code here if needed
-		super.update();
+		//super.update();
 	}
 }
