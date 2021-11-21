1122fcce7e5b32500a631915c4694c765a63f329
Zac Waite <129032699zw@gmail.com>
2021-11-21 17:17:36 -0500

    Response class

---
+++ src/helpers/response.js
@@ -0,0 +1,9 @@
+export default class response {
+    constructor(status=400, errors=[], response={}, success=false) {
+        this.success = success;
+        this.status = status;
+        this.errors = [];
+        this.errors.push(...errors);
+        this.response = response;
+    }
+}
