b345c833d67bc18342f9dd4ee4eced159196d18e
Zac Waite <129032699zw@gmail.com>
2022-02-04 13:28:05 -0500

    Organized Class Attributes Parent Classes

--- src/sprite.js
+++ src/sprite.js
@@ -1,19 +1,17 @@
 import Vector2 from "./helpers/Vector2.js";
 import RenderedObject from "./renderedObject.js";
 export default class Sprite extends RenderedObject{
-	radius;
-	mass;
+	/* Default Attributes */
+	speed = new Vector2(0, 0); //linear speed
+	accel = new Vector2(0, 0); //linear acceleration
+	maxSpeed = 100; //Max linear speed
+	aSpeed = new Vector2(1, 0); //angle speed
+	aAccel = new Vector2(1, 0); //angle acceleration
+	maxASpeed = 20; //Max anglular speed
+	radius = 1; // Object collision Radius
+	mass = 1; //Object mass for collisions
 	constructor(...args){
 		super(...args)
-		// this.pos = new Vector(0, 0); --> Already from RenderedObject
-		this.speed = new Vector2(0, 0); //linear speed
-		this.accel = new Vector2(0, 0); //linear acceleration
-		this.maxSpeed = 100; //Max linear speed
-		// this.angle = 0; --> Already, from RenderedObject
-		this.aSpeed = new Vector2(1, 0); //angle speed
-		this.aAccel = new Vector2(1, 0); //angle acceleration
-		//console.log(this.aAccel.angle());
-		this.maxASpeed = 20; //Max anglular speed
 	}
 	update() {
 		// All Simple Physics
