b76257b68958fa2a6af9c206b94ceb47a04c72ff
Matthew Zhou <myzhou@uwaterloo.ca>
2022-09-15 18:23:04 -0400

    add angleDiff

--- typescript/students/Bebop/PropulsionController.ts
+++ typescript/students/Bebop/PropulsionController.ts
@@ -5,18 +5,22 @@ import YourDefenceController from './DefenseController.js'
 import YourNavigationController from './NavigationController.js'
 import YourSensorsController from './SensorsController.js'
 export default class YourPropulsionController extends PropulsionController {
-	// To get other subsystem information, use the attributes below.
-	// @ts-ignore
-	defence: YourDefenceController // @ts-ignore
-	sensors: YourSensorsController // @ts-ignore
-	navigation: YourNavigationController
+    // To get other subsystem information, use the attributes below.
+    // @ts-ignore
+    defence: YourDefenceController // @ts-ignore
+    sensors: YourSensorsController // @ts-ignore
+    navigation: YourNavigationController
 
-	//Add additional attributes here
+    //Add additional attributes here
 
-	propulsionUpdate(setThruster: (thruster: ThrusterName, power: number) => Error | null) {
-		if (!this.sensors.target) return
-        
-        const headingDiff = angleDiff(this.navigation.angle, this.sensors.target.heading)
+    angleDiff(a: number, b: number): number {
+        return (a - b);
+    }
+
+    propulsionUpdate(setThruster: (thruster: ThrusterName, power: number) => Error | null) {
+        if (!this.sensors.target) return
+
+        const headingDiff = this.angleDiff(this.navigation.angle, this.sensors.target.heading)
         const force = Math.min(Math.abs(500 * headingDiff), 100)
 
         if (headingDiff < 0) {
@@ -29,5 +33,5 @@ export default class YourPropulsionController extends PropulsionController {
         }
 
         setThruster('main', Math.abs(headingDiff) < 0.2 ? 30 : 0)
-	}
+    }
 }
