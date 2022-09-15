7459326d729bcfe74650d0aec2d2b89c927ba641
emizrahi <emizrahi@uwaterloo.ca>
2022-09-15 17:48:29 -0400

    Hey guys

--- typescript/students/Normandy/SensorsController.ts
+++ typescript/students/Normandy/SensorsController.ts
@@ -16,4 +16,9 @@ export default class YourSensorsController extends SensorsController {
 	sensorsUpdate(activeScan: (heading: number, arc: number, range: number) => EMSReading[] | Error, passiveScan: () => PassiveReading[] | Error) {
 		//Student code goes here
 	}
+	sensorsUpdate(){
+		const scanResult = passiveScan()
+		if(!(scanResult instanceof Error )) this.target = scanResult[0]
+	
+	}
 }
