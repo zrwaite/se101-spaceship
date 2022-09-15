edaaa89d196317108b046f97407793e54c25b9f2
Alysha Wang <avwang@uwaterloo.ca>
2022-09-15 18:11:21 -0400

    add angle property

--- typescript/students/FlyingDutchman/NavigationController.ts
+++ typescript/students/FlyingDutchman/NavigationController.ts
@@ -12,6 +12,7 @@ export default class YourNavigationController extends NavigationController {
 	defence: YourDefenceController // @ts-ignore
 	sensors: YourSensorsController // @ts-ignore
 	propulsion: YourPropulsionController
+	angle: number = 0
 
 	//Add additional attributes here
 
