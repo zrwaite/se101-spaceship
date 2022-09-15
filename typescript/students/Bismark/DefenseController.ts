b713f5bdd16810e05a5e8295fd06180b2ec793d1
Alakh <36898190+alakhpc@users.noreply.github.com>
2022-09-15 18:05:54 -0400

    Fix some errors

--- typescript/students/Bismark/DefenseController.ts
+++ typescript/students/Bismark/DefenseController.ts
@@ -1,21 +1,26 @@
-import DefenceController from '../../src/subsystems/defenceController.js'
-import { Vector2, withinPiRange, angleDiff } from '../helpers.js'
-import YourNavigationController from './NavigationController.js'
-import YourPropulsionController from './PropulsionController.js'
-import YourSensorsController from './SensorsController.js'
+import DefenceController from "../../src/subsystems/defenceController.js";
+import { Vector2, withinPiRange, angleDiff } from "../helpers.js";
+import YourNavigationController from "./NavigationController.js";
+import YourPropulsionController from "./PropulsionController.js";
+import YourSensorsController from "./SensorsController.js";
 export default class YourDefenceController extends DefenceController {
-	// To get other subsystem information, use the attributes below. 
-	// @ts-ignore
-	navigation: YourNavigationController // @ts-ignore
-	sensors: YourSensorsController // @ts-ignore
-	propulsion: YourPropulsionController
-	//Add additional attributes here
+  // To get other subsystem information, use the attributes below.
+  // @ts-ignore
+  navigation: YourNavigationController; // @ts-ignore
+  sensors: YourSensorsController; // @ts-ignore
+  propulsion: YourPropulsionController;
+  //Add additional attributes here
 
-	target: number;
-	
-	defenceUpdate(aimTurret: (angle: number) => void, getTubeCooldown: (i: number) => number | Error, fireTorpedo: (i: number) => Error | null) {
-		if (!this.sensors.target) return
-		aimTurret(this.sensors.target.heading)
-		fireTorpedo(0)
-	}
+  // @ts-ignore
+  target: number;
+
+  defenceUpdate(
+    aimTurret: (angle: number) => void,
+    getTubeCooldown: (i: number) => number | Error,
+    fireTorpedo: (i: number) => Error | null
+  ) {
+    if (!this.sensors.target) return;
+    aimTurret(this.sensors.target.heading);
+    fireTorpedo(0);
+  }
 }
--- typescript/students/Bismark/PropulsionController.ts
+++ typescript/students/Bismark/PropulsionController.ts
@@ -1,31 +1,35 @@
-import { Vector2, withinPiRange } from '../helpers.js'
-import { ThrusterName } from '../types.js'
-import PropulsionController from '../../src/subsystems/propulsionController.js'
-import YourDefenceController from './DefenseController.js'
-import YourNavigationController from './NavigationController.js'
-import YourSensorsController from './SensorsController.js'
+import { Vector2, withinPiRange, angleDiff } from "../helpers.js";
+import { ThrusterName } from "../types.js";
+import PropulsionController from "../../src/subsystems/propulsionController.js";
+import YourDefenceController from "./DefenseController.js";
+import YourNavigationController from "./NavigationController.js";
+import YourSensorsController from "./SensorsController.js";
 export default class YourPropulsionController extends PropulsionController {
-	// To get other subsystem information, use the attributes below.
-	// @ts-ignore
-	defence: YourDefenceController // @ts-ignore
-	sensors: YourSensorsController // @ts-ignore
-	navigation: YourNavigationController
+  // To get other subsystem information, use the attributes below.
+  // @ts-ignore
+  defence: YourDefenceController; // @ts-ignore
+  sensors: YourSensorsController; // @ts-ignore
+  navigation: YourNavigationController;
 
-	//Add additional attributes here
+  //Add additional attributes here
 
-	propulsionUpdate(setThruster: (thruster: ThrusterName, power: number) => Error | null) {
-		//Student code goes here
-		if (!this.sensors.target) return
-		const headingDiff = angleDiff(this.navigation.angle, this.sensors.target.heading)
-		const force = Math.min(Math.abs(500 * headingDiff), 100)
-		if (headingDiff < 0) {
-			setThruster('clockwise', force)
-			setThruster('counterClockwise', 0)
-		}
-		else {
-			setThruster('counterClockwise', force)
-			setThruster('clockwise', 0)
-		}
-		setThruster('main', Math.abs(headingDiff)< 0.2 ? 30 : 0)
-	}
+  propulsionUpdate(
+    setThruster: (thruster: ThrusterName, power: number) => Error | null
+  ) {
+    //Student code goes here
+    if (!this.sensors.target) return;
+    const headingDiff = angleDiff(
+      this.navigation.angle,
+      this.sensors.target.heading
+    );
+    const force = Math.min(Math.abs(500 * headingDiff), 100);
+    if (headingDiff < 0) {
+      setThruster("clockwise", force);
+      setThruster("counterClockwise", 0);
+    } else {
+      setThruster("counterClockwise", force);
+      setThruster("clockwise", 0);
+    }
+    setThruster("main", Math.abs(headingDiff) < 0.2 ? 30 : 0);
+  }
 }
