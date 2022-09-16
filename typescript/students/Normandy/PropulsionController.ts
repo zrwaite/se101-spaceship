cc87291fbbb743b974f7f480b1bc0a3b688759d2
Daniel <yedaniel99@gmail.com>
2022-09-16 10:47:51 -0400

    formatting PropulsionController.ts file

--- typescript/students/Normandy/PropulsionController.ts
+++ typescript/students/Normandy/PropulsionController.ts
@@ -5,26 +5,30 @@ import YourDefenceController from './DefenseController.js'
 import YourNavigationController from './NavigationController.js'
 import YourSensorsController from './SensorsController.js'
 export default class YourPropulsionController extends PropulsionController {
-	// To get other subsystem information, use the attributes below.
-	// @ts-ignore
-	defence: YourDefenceController // @ts-ignore
-	sensors: YourSensorsController // @ts-ignore
-	navigation: YourNavigationController
+  // To get other subsystem information, use the attributes below.
+  // @ts-ignore
+  defence: YourDefenceController; // @ts-ignore
+  sensors: YourSensorsController; // @ts-ignore
+  navigation: YourNavigationController;
 
-	//Add additional attributes here
+  //Add additional attributes here
 
-	propulsionUpdate(setThruster: (thruster: ThrusterName, power: number) => Error | null) {
-		if(!this.sensors.target) return
-		const headingDiff = angleDiff(this.navigation.angle, this.sensors.target.heading)
-		const force = Math.min(Math.abs(500*headingDiff), 100)
-		
-		if (headingDiff < 0){
-			setThruster('clockwise', force)
-			setThruster('counterClockwise', 0)
-		} else {
-			setThruster('counterClockwise', force)
-			setThruster('clockwise', 0)
-		}
-		setThruster('main', Math.abs(headingDiff) < 0.2 ? 30 : 0)
-	}
+  propulsionUpdate(
+    setThruster: (thruster: ThrusterName, power: number) => Error | null
+  ) {
+    if (!this.sensors.target) return;
+    const headingDiff = angleDiff(
+      this.navigation.angle,
+      this.sensors.target.heading
+    );
+    const force = Math.min(Math.abs(500 * headingDiff), 100);
+    if (headingDiff < 0) {
+      setThruster("clockwise", force);
+      setThruster("counterClockwise", 0);
+    } else {
+      setThruster("counterClockwise", force);
+      setThruster("clockwise", 0);
+    }
+    setThruster("main", Math.abs(headingDiff) < 0.2 ? 30 : 0);
+  }
 }
