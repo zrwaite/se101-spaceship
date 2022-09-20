22763e04311d1f9903710280cc33bb0ef1dd74ae
DongtaiS <dongtaishi.w@gmail.com>
2022-09-20 15:58:55 -0400

    Commenting out unknown import

--- typescript/students/Normandy/SensorsController.ts
+++ typescript/students/Normandy/SensorsController.ts
@@ -4,7 +4,7 @@ import YourDefenceController from './DefenseController.js'
 import YourNavigationController from './NavigationController.js'
 import YourPropulsionController from './PropulsionController.js'
 import { EMSReading, PassiveReading } from '../types.js'
-import { isPrivateIdentifier } from '../../../node_modules/typescript/lib/typescript.js'
+//import { isPrivateIdentifier } from '../../../node_modules/typescript/lib/typescript.js'
 export default class YourSensorsController extends SensorsController {
 	// To get other subsystem information, use the attributes below.
 	// @ts-ignore
