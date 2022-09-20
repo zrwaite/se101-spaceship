f1d468cdcc8b66ce8603360f1966baf4ac18d90d
Nicole Jin <nicole.qianhui.jin@gmail.com>
2022-09-20 15:23:41 -0400

    Add design plan in comments

--- typescript/students/FlyingDutchman/SensorsController.ts
+++ typescript/students/FlyingDutchman/SensorsController.ts
@@ -19,3 +19,11 @@ export default class YourSensorsController extends SensorsController {
 		if (!(scanResult instanceof Error)) this.target = scanResult[0]
 	}
 }
+
+
+// accurate vector coordinates (angle, distance) of warpgates and planets - navigation
+// angle, heading, velocity, distance of asteroids - defense
+
+// passive scan - heading, mass/distance - warpgates accurate position
+// active scan - angle, distance, velocity, radius
+				// close range - type, habitability
