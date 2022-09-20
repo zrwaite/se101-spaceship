8a30d4743e68eb6c95ce2de7dd6332a16d189f09
Kevin Liu <k36liu@uwaterloo.ca>
2022-09-20 18:01:40 -0400

    simpler propulsion algorithm lol

--- javascript/students/PillarOfAutumn/PropulsionController.js
+++ javascript/students/PillarOfAutumn/PropulsionController.js
@@ -6,21 +6,11 @@ export default class YourPropulsionController extends PropulsionController {
         if (!this.sensors.target)
             return;
         const headingDiff = withinPiRange(angleDiff(this.navigation.angle, this.sensors.target.heading));
-        const force = Math.min(100 * Math.abs(headingDiff), 10);
+        const force = 1;
         console.log(headingDiff);
-        setThruster('main', force);
-        if (headingDiff < 0 && Math.abs(headingDiff) >= 0.5) {
+        if (Math.abs(headingDiff) != 0) {
             setThruster('clockwise', force);
-            setThruster('counterClockwise', 0);
         }
-        else if (headingDiff > 0 && Math.abs(headingDiff) >= 0.5) {
-            setThruster('counterClockwise', force);
-            setThruster('clockwise', 0);
-        }
-        else {
-            setThruster('counterClockwise', 0);
-            setThruster('clockwise', 0);
-        }
-        setThruster('main', Math.abs(headingDiff) < 0.5 ? 50 : 0);
+        setThruster('main', Math.abs(headingDiff) < 0.2 ? 100 : 0);
     }
 }
