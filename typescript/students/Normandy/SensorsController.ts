cf80b5a116e0101fec1f34a707697c290dee25c7
Joe Yu <j56yu@uwaterloo.ca>
2022-09-20 16:36:10 -0400

    return land target correctly

--- typescript/students/Normandy/SensorsController.ts
+++ typescript/students/Normandy/SensorsController.ts
@@ -14,28 +14,40 @@ export default class YourSensorsController extends SensorsController {
 	
 	//Add additional attributes here
 	target: PassiveReading | null=null;
-	landTarget: PassiveReading | null=null;
+	landTarget: Vector2 | null=null;
+
   	collisionCheck(targetX: number, targetY: number, selfVelocity: Vector2, targetVelocity: Vector2): boolean {
 		return false;
 	}
-	cartesian(angle: number, distance: number): number[] {
+	cartesian(angle: number, distance: number): Vector2 {
 		// Given angle and distance of an object, return x,y cords assuming ship pos is 0,0
-		return [distance*Math.cos(angle), distance*Math.sin(angle)]
+		return new Vector2(distance*Math.cos(angle), distance*Math.sin(angle))
 
  	} 
 
- 	polar(x: number, y: number): number[]{
+ 	polar(x: number, y: number): Vector2{
  		// Given x,y coords assuming ship pos is 0,0, return [angle (in radians), distance]
- 		return [Math.sqrt(x*x + y*y), Math.atan(y/x)]
+ 		return new Vector2(Math.sqrt(x*x + y*y), Math.atan(y/x))
  	}
 
  	sensorsUpdate(activeScan: (heading: number, arc: number, range: number) => EMSReading[] | Error, passiveScan: () => PassiveReading[] | Error) {
  		//Student code goes here
  		const scanResult = passiveScan();
 		
+		this.landTarget = new Vector2(360,270)
+
 		if(!(scanResult instanceof Error)) {
 			this.target = scanResult[0];
-	
+			for(let i=0; i<scanResult.length; i++){
+				const ems = activeScan(scanResult[i].heading,0.1,680)
+				if(!(ems instanceof Error)) {
+					for(let j=0; i<ems.length; i++){
+						if(ems[j].distance>0 && ems[j].radius<=45 && ems[j].radius>=25){
+							this.landTarget = this.cartesian(ems[j].angle, ems[j].distance)
+						}
+					}
+				}
+			}
 		}
 	}
 }
