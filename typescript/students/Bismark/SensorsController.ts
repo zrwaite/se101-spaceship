666abd76eedabd08e78bf5d7ec6fe0415cd03d23
Sophia Sun <53122458+sophiiasun@users.noreply.github.com>
2022-09-20 15:21:42 -0400

    Updated Sensors File with Github Sample Code

--- typescript/students/Bismark/SensorsController.ts
+++ typescript/students/Bismark/SensorsController.ts
@@ -4,6 +4,7 @@ import YourDefenceController from "./DefenseController.js";
 import YourNavigationController from "./NavigationController.js";
 import YourPropulsionController from "./PropulsionController.js";
 import { EMSReading, PassiveReading } from "../types.js";
+import { CloseRangeData } from "../../src/ship/EMSReading.js";
 export default class YourSensorsController extends SensorsController {
   // To get other subsystem information, use the attributes below.
   // @ts-ignore
@@ -25,5 +26,25 @@ export default class YourSensorsController extends SensorsController {
   ) {
     const scanResult = passiveScan();
     if (!(scanResult instanceof Error)) this.target = scanResult[0];
+    
+    interface EMSReading {
+      angle: number
+      distance: number
+      velocity: Vector2
+      radius: number
+      closeRange?: CloseRangeData
+    }
+
+    interface CloseRangeData {
+      type: 'Planet' | 'Meteor' | 'Asteroid' | 'WarpGate' | 'Other' 
+      planetComposition?: {
+        water:number
+        air:number
+        land:number
+        metal:number
+        safety:number
+        temperature:number
+      }
+    }
   }
 }
