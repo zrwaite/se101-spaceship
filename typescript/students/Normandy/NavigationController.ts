80744af22f2055db367dd240711f345623fe24ff
Cheney Cai <cheneycai2004@hotmail.com>
2022-09-20 15:25:27 -0400

    planets and warpgates in nav

--- typescript/students/Normandy/NavigationController.ts
+++ typescript/students/Normandy/NavigationController.ts
@@ -5,6 +5,8 @@ import NavigationController from '../../src/subsystems/navigationController.js'
 import YourDefenceController from './DefenseController.js'
 import YourPropulsionController from './PropulsionController.js'
 import YourSensorsController from './SensorsController.js'
+import WarpGate from '../../src/spaceObjects/warpGate.js'
+import { NumberLiteralType } from '../../../node_modules/typescript/lib/typescript.js'
 
 export default class YourNavigationController extends NavigationController {
 	// To get other subsystem information, use the attributes below.
@@ -17,8 +19,29 @@ export default class YourNavigationController extends NavigationController {
 	angle: number = 0;
 
 	navigationUpdate(getShipStatus: (key: keyof ShipStatus) => number, warp: () => Error|null, land: () => Error|null, getMapData: () => MapData) {
-		//Student code goes here
-		this.angle = getShipStatus('angle')
-		land();
+
+		
+		
+		//what the DUCK is a planet
+		interface Planet {
+			x: number
+			y: number
+			planetComposition: {
+				water: number
+				air: number
+				land: number
+				metal: number
+				safety: number
+				temperature: number
+			}
+		}
+
+		//what the DUCK is a wormhole
+		interface WarpGate {
+			x: number
+			y: number
+		}
+
 	}
+
 }
