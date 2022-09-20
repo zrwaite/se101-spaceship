790ddd9047d39bb78ddc5d1bd88b32568ff911f4
Derek Li <rejie.li@gmail.com>
2022-09-20 15:11:36 -0400

    moved code to typescript

--- typescript/students/EventHorizon/SensorsController.ts
+++ typescript/students/EventHorizon/SensorsController.ts
@@ -15,5 +15,7 @@ export default class YourSensorsController extends SensorsController {
 	
 	sensorsUpdate(activeScan: (heading: number, arc: number, range: number) => EMSReading[] | Error, passiveScan: () => PassiveReading[] | Error) {
 		//Student code goes here
+		const scanResult = passiveScan()
+		if (!(scanResult instanceof Error)) this.target = scanResult[0]
 	}
 }
