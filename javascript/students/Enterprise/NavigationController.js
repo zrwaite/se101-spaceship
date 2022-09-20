4c2c10dd81b672d474d60bc7ac8dcb550305a617
Zain <zainsalman04@gmail.com>
2022-09-20 16:49:45 -0400

    changed closeRangeObject to be an array

--- javascript/students/Enterprise/NavigationController.js
+++ javascript/students/Enterprise/NavigationController.js
@@ -7,8 +7,11 @@ export default class YourNavigationController extends NavigationController {
     navigationUpdate(getShipStatus, warp, land, getMapData) {
         const closeRangeObject = this.sensors.closeRangeObject;
         // Landing if the distance between the spaceship and A planet is less than 20
-        if ((closeRangeObject === null || closeRangeObject === void 0 ? void 0 : closeRangeObject.closeRange) && closeRangeObject.closeRange.type === 'Planet' && closeRangeObject.distance < 20) {
+        if ((closeRangeObject === null || closeRangeObject === void 0 ? void 0 : closeRangeObject.closeRange) && closeRangeObject.closeRange.type === "Planet" && closeRangeObject.distance < 20) {
             land();
+        }
+        if ((closeRangeObject === null || closeRangeObject === void 0 ? void 0 : closeRangeObject.closeRange) && closeRangeObject.closeRange.type === "WarpGate" && closeRangeObject.distance < 20) {
+            warp();
         }
         this.angle = getShipStatus("angle");
     }
