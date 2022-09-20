c8ca86c89b6641e84690214a714f5c6dd085210a
Joe Yu <j56yu@uwaterloo.ca>
2022-09-20 14:52:42 -0400

    added collision check function

--- typescript/students/Normandy/SensorsController.ts
+++ typescript/students/Normandy/SensorsController.ts
@@ -14,9 +14,17 @@ export default class YourSensorsController extends SensorsController {
 	//Add additional attributes here
 	target: PassiveReading | null=null;
 
+  	collisionCheck(): boolean {
+		return false;
+	}
+
 	sensorsUpdate(activeScan: (heading: number, arc: number, range: number) => EMSReading[] | Error, passiveScan: () => PassiveReading[] | Error) {
 		//Student code goes here
 		const scanResult = passiveScan();
-		if(!(scanResult instanceof Error)) this.target = scanResult[0];
+
+		if(!(scanResult instanceof Error)) {
+			this.target = scanResult[0];
+		
+		}
 	}
 }
