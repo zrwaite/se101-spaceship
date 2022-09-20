48546691a617819e710cea4c57d594c6f466cb3d
Donghwui <donghwuikim0102@gmail.com>
2022-09-20 16:38:16 -0400

    warp function required

--- typescript/students/PillarOfAutumn/NavigationController.ts
+++ typescript/students/PillarOfAutumn/NavigationController.ts
@@ -5,8 +5,8 @@ import NavigationController from '../../src/subsystems/navigationController.js'
 import YourDefenceController from './DefenseController.js'
 import YourPropulsionController from './PropulsionController.js'
 import YourSensorsController from './SensorsController.js'
-let planet = false;
-let portal = false;
+let planet = true;
+let portal = true;
 
 export default class YourNavigationController extends NavigationController {
 	// To get other subsystem information, use the attributes below.
@@ -18,15 +18,7 @@ export default class YourNavigationController extends NavigationController {
 
 	navigationUpdate(getShipStatus: (key: keyof ShipStatus) => number, warp: () => Error|null, land: () => Error|null, getMapData: () => MapData) {
 		this.angle = getShipStatus('angle')
-		//figure out how to read mapdata
-		getMapData();
-		if(planet){
-			land();
-			planet = false;
-		}
-		if (portal){
-			warp();
-			portal = false;
-		}
+		land()
+		// needs a function to warp when the spaceship is on the warpgate
 	}
 }
