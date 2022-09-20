236ceb0748ce28da8cddafb60b7d928336b8eb0d
Eric Zhang <erizha.grad2022@my.stmc.bc.ca>
2022-09-20 14:46:56 -0400

    fixed some heading issues

--- javascript/students/Enterprise/PropulsionController.js
+++ javascript/students/Enterprise/PropulsionController.js
@@ -1,10 +1,11 @@
 import PropulsionController from '../../src/subsystems/propulsionController.js';
+import { angleDiff } from '../helpers.js';
 export default class YourPropulsionController extends PropulsionController {
     //Add additional attributes here
     propulsionUpdate(setThruster) {
         if (!this.sensors.target)
             return;
-        const headingDif = angleDiff(this.navigation.angle, this.sensors.target.heading);
+        const headingDif = angleDiff(this.sensors.target.heading, this.sensors.target.heading);
         const force = Math.min(Math.abs(500 * headingDif), 100);
         if (headingDif < 0) {
             setThruster('clockwise', force);
@@ -14,6 +15,6 @@ export default class YourPropulsionController extends PropulsionController {
             setThruster('counterClockwise', force);
             setThruster('clockwise', 0);
         }
-        setThruster('main', Math.abs(headingDiff) < 0.2 ? 30 : 0);
+        setThruster('main', Math.abs(headingDif) < 0.2 ? 30 : 0);
     }
 }
--- typescript/students/Enterprise/PropulsionController.ts
+++ typescript/students/Enterprise/PropulsionController.ts
@@ -4,6 +4,8 @@ import PropulsionController from '../../src/subsystems/propulsionController.js'
 import YourDefenceController from './DefenseController.js'
 import YourNavigationController from './NavigationController.js'
 import YourSensorsController from './SensorsController.js'
+
+import { angleDiff } from '../helpers.js'
 export default class YourPropulsionController extends PropulsionController {
 	// To get other subsystem information, use the attributes below.
 	// @ts-ignore
@@ -15,7 +17,9 @@ export default class YourPropulsionController extends PropulsionController {
 
 	propulsionUpdate(setThruster: (thruster: ThrusterName, power: number) => Error | null) {
 		if (!this.sensors.target) return
-		const headingDif = angleDiff(this.navigation.angle, this.sensors.target.heading)
+
+		const headingDif = angleDiff(this.sensors.target.heading, this.sensors.target.heading)
+
 		const force = Math.min(Math.abs(500 * headingDif), 100)
 		if (headingDif < 0) {
 			setThruster('clockwise', force)
@@ -24,7 +28,7 @@ export default class YourPropulsionController extends PropulsionController {
 			setThruster('counterClockwise', force)
 			setThruster('clockwise', 0)
 		}
-		setThruster('main', Math.abs(headingDiff) < 0.2 ? 30 : 0)
+		setThruster('main', Math.abs(headingDif) < 0.2 ? 30 : 0)
 	}
 	//Hello
 }
