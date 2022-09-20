04c5aa2559cf4fb3c8f83d955beb03e9f64bc56d
Ethan Lim <limh8275@wrdsb.ca>
2022-09-20 17:17:55 -0400

    started work on mapping items

--- javascript/students/Enterprise/SensorsController.js
+++ javascript/students/Enterprise/SensorsController.js
@@ -5,12 +5,27 @@ export default class YourSensorsController extends SensorsController {
         //Add additional attributes here
         this.target = null;
         this.closeRangeObject = null;
+        this.ready = false;
+        this.destinations = null;
     }
     sensorsUpdate(activeScan, passiveScan) {
         const scanResult = passiveScan();
         const activeResult = this.target ? activeScan(this.target.heading - 3.14 / 2, 3.14, 200) : new Error("not ready");
-        if (!(scanResult instanceof Error))
-            this.target = scanResult[0];
+        if (!(scanResult instanceof Error)) {
+            if (!this.ready) {
+                for (const item of scanResult) {
+                    let initActiveRes = activeScan(item.heading - 3.14 / 10000, 3.14 / 5000, 720);
+                    if (!(initActiveRes instanceof Error)) {
+                        for (const spaceObj of initActiveRes) {
+                            if (spaceObj.radius > 25 && spaceObj.radius < 45 && spaceObj.distance * item.gravity > 2500) {
+                            }
+                        }
+                    }
+                }
+            }
+            else
+                this.target = scanResult[0];
+        }
         if (!(activeResult instanceof Error)) {
             this.closeRangeObject = activeResult[0];
         }
--- typescript/students/Enterprise/SensorsController.ts
+++ typescript/students/Enterprise/SensorsController.ts
@@ -4,6 +4,11 @@ import YourDefenceController from './DefenseController.js'
 import YourNavigationController from './NavigationController.js'
 import YourPropulsionController from './PropulsionController.js'
 import { EMSReading, PassiveReading } from '../types.js'
+type Destination = {
+	type: string;
+	radius: number;
+	mass: number;
+}
 export default class YourSensorsController extends SensorsController {
 	// To get other subsystem information, use the attributes below.
 	// @ts-ignore
@@ -11,16 +16,38 @@ export default class YourSensorsController extends SensorsController {
 	navigation: YourNavigationController // @ts-ignore
 	propulsion: YourPropulsionController
 
+
 	//Add additional attributes here
 	target: PassiveReading | null = null;
-  closeRangeObject: EMSReading | null = null;
+  	closeRangeObject: EMSReading | null = null;
+	ready: boolean = false;
+	destinations: Destination[] | null = null;
+
+
+
 	sensorsUpdate(activeScan: (heading: number, arc: number, range: number) => EMSReading[] | Error, passiveScan: () => PassiveReading[] | Error) {
+	
 		const scanResult = passiveScan();
-    const activeResult = this.target ? activeScan(this.target.heading - 3.14/2, 3.14, 200) : new Error("not ready");
-    if(!(scanResult instanceof Error)) this.target = scanResult[0];
-    if(!(activeResult instanceof Error)) {
-      this.closeRangeObject = activeResult[0];
-    }
-    this.closeRangeObject && console.log(this.closeRangeObject);
+
+		const activeResult = this.target ? activeScan(this.target.heading - 3.14/2, 3.14, 200) : new Error("not ready");
+		if(!(scanResult instanceof Error)) {		
+			if(!this.ready) {
+				for (const item of scanResult) {
+					let initActiveRes = activeScan(item.heading - 3.14/10000, 3.14/5000, 720)
+					if(!(initActiveRes instanceof Error)) {
+						for (const spaceObj of initActiveRes) {
+							if(spaceObj.radius > 25 && spaceObj.radius < 45 && spaceObj.distance * item.gravity > 2500) {
+
+							}
+						}
+					}
+				}
+			}
+			else	this.target = scanResult[0];
+		}
+		if(!(activeResult instanceof Error)) {
+		this.closeRangeObject = activeResult[0];
+		}
+		this.closeRangeObject && console.log(this.closeRangeObject);
   }
 }
