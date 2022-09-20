348348db64bd406166049d228bdbead71077fe43
DongtaiS <dongtaishi.w@gmail.com>
2022-09-20 16:26:38 -0400

    Fixing navigation fr this time

--- javascript/students/Normandy/NavigationController.js
+++ javascript/students/Normandy/NavigationController.js
@@ -3,9 +3,22 @@ import NavigationController from '../../src/subsystems/navigationController.js';
 export default class YourNavigationController extends NavigationController {
     constructor() {
         super(...arguments);
-        //Add additional attributes here
-        this.angle = 0;
+        //PUBLIC VARIABLES :)
+        this.radius = 0; //how big the ship is
+        this.angularVelocity = 0;
+        this.angle = 0; //direction ship is facing
+        this.positionX = 0;
+        this.positionY = 0;
+        this.linearVelocityX = 0;
+        this.linearVelocityY = 0;
+        this.thrusterPowerMain = 0;
+        this.thrusterPowerBow = 0;
+        this.thrusterPowerClockwise = 0;
+        this.thrusterPowerCounterClockwise = 0;
     }
+    //runs multiple times a second
     navigationUpdate(getShipStatus, warp, land, getMapData) {
+        this.angle = getShipStatus('angle');
+        land();
     }
 }
