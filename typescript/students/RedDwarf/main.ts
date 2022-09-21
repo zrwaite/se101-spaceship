81857a9f67e95bcde6bf0ae2c78db32cc602560c
ProximityTrigger <proximitytrigger@gmail.com>
2022-09-21 14:51:56 -0400

    <diep> !

--- typescript/students/RedDwarf/main.ts
+++ typescript/students/RedDwarf/main.ts
@@ -1,4 +1,11 @@
+const galaxies = document.querySelectorAll('td[id^=galaxy]');
 (document.querySelector('#Galaxy > table') as HTMLTableElement).addEventListener('dblclick', function(this: HTMLElement, e: MouseEvent){
-    if (document.querySelector('figure.active')?.id === 'Red Dwarf' && !(e.target as HTMLElement).matches('td'))
-        window.open('https://diep.io', '_blank');
+    if (document.querySelector('figure.active')?.id === 'Red Dwarf') {
+        const rect = galaxies[0].getBoundingClientRect();
+        const boxL = rect.left + rect.width, boxT = rect.top + rect.height;
+        const rect2 = galaxies[3].getBoundingClientRect();
+        const boxR = rect2.left, boxB = rect2.top;
+        if (e.pageX >= boxL && e.pageX <= boxR && e.pageY >= boxT && e.pageY <= boxB)
+            window.open('https://diep.io', '_blank');
+    }
 });
