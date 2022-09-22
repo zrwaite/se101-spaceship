5f17bc0a64bbdc3e4b22959bd7c4462cb09c8905
Ethan Lim <hyeonseong04.lim@gmail.com>
2022-09-22 16:16:41 -0400

    deleted some stuff

--- typescript/students/Enterprise/SensorsController.ts
+++ typescript/students/Enterprise/SensorsController.ts
@@ -31,19 +31,20 @@ export default class YourSensorsController extends SensorsController {
 
 		const activeResult = this.target ? activeScan(this.target.heading - 3.14/2, 3.14, 200) : new Error("not ready");
 		if(!(scanResult instanceof Error)) {		
-			if(!this.ready) {
-				for (const item of scanResult) {
-					let initActiveRes = activeScan(item.heading - 3.14/10000, 3.14/5000, 720)
-					if(!(initActiveRes instanceof Error)) {
-						for (const spaceObj of initActiveRes) {
-							if(spaceObj.radius > 25 && spaceObj.radius < 45 && spaceObj.distance * item.gravity > 2500) {
-
-							}
-						}
-					}
-				}
-			}
-			else	this.target = scanResult[0];
+			// if(!this.ready) {
+			// 	for (const item of scanResult) {
+			// 		let initActiveRes = activeScan(item.heading - 3.14/10000, 3.14/5000, 720)
+			// 		if(!(initActiveRes instanceof Error)) {
+			// 			for (const spaceObj of initActiveRes) {
+			// 				if(spaceObj.radius > 25 && spaceObj.radius < 45 && spaceObj.distance * item.gravity > 2500) {
+								
+			// 				}
+			// 			}
+			// 		}
+			// 	}
+			// }
+			// else	
+			this.target = scanResult[0];
 		}
     if(!(scanResult instanceof Error)) this.target = scanResult[0];
 		if(!(activeResult instanceof Error)) {
