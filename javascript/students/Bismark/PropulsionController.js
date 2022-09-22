e3437bb7f656c70a32f1262e59b6199fb7143455
okiita <melideng123@gmail.com>
2022-09-22 16:23:00 -0400

    smaller active scan range

--- javascript/students/Bismark/PropulsionController.js
+++ javascript/students/Bismark/PropulsionController.js
@@ -11,14 +11,14 @@ export default class YourPropulsionController extends PropulsionController {
         // Speed by getting magnitude
         const speed = Math.pow(((_a = this.navigation.linearVelocityX) !== null && _a !== void 0 ? _a : 0) ** 2 +
             ((_b = this.navigation.linearVelocityY) !== null && _b !== void 0 ? _b : 0) ** 2, 1 / 2);
-        const correctionalForce = 250;
+        const correctionalForce = 250; //to compensate power on one side
         const headingDiff = angleDiff(this.navigation.angle, this.sensors.target.heading);
         const force = Math.min(Math.abs(100 * headingDiff), 50);
         const headingDiffBuffer = 0;
         if (headingDiff < -headingDiffBuffer) {
             if (this.prevHeadingDiff &&
                 this.prevHeadingDiff - headingDiff < -0.001 && // If the spaceship is turning "fast"
-                headingDiff >= -0.5 // If almost pointing at the right direction
+                headingDiff >= -0.5 // If almost pointing at the right direction 
             ) {
                 // Thrust other way
                 setThruster("counterClockwise", correctionalForce);
@@ -45,6 +45,7 @@ export default class YourPropulsionController extends PropulsionController {
             setThruster("counterClockwise", 0);
             setThruster("clockwise", 0);
         }
+        setThruster("bow", Math.abs(headingDiff) > 0.2 && Math.abs(speed) > 0 ? 30 : 0);
         setThruster("main", Math.abs(headingDiff) < 0.2 && speed < 0.75 ? 30 : 0);
         this.prevHeadingDiff = headingDiff;
         this.prevSpeed = speed;
--- typescript/students/Bismark/SensorsController.ts
+++ typescript/students/Bismark/SensorsController.ts
@@ -5,6 +5,9 @@ import YourNavigationController from "./NavigationController.js";
 import YourPropulsionController from "./PropulsionController.js";
 import { EMSReading, PassiveReading } from "../types.js";
 import { CloseRangeData } from "../../src/ship/EMSReading.js";
+import { buildShip } from "../../src/ship/buildShip.js";
+import ColonyShip from "../../src/ship/colonyShip.js";
+import ActiveSensors from "../../src/ship/activeSensors.js";
 export default class YourSensorsController extends SensorsController {
   // To get other subsystem information, use the attributes below.
   // @ts-ignore
@@ -34,11 +37,8 @@ export default class YourSensorsController extends SensorsController {
     const scanResult = passiveScan();
     if (!(scanResult instanceof Error)) this.target = scanResult[0];
 
-    const activeScanResult = activeScan(
-      this.navigation.angle - Math.PI / 2,
-      Math.PI,
-      100
-    );
+    const activeScanResult = activeScan(this.navigation.angle - Math.PI / 8, Math.PI, 100);
+
     if (!(activeScanResult instanceof Error)) {
       this.targetDetails = activeScanResult.find(
         (r) => r.angle === this.target?.heading
@@ -50,5 +50,7 @@ export default class YourSensorsController extends SensorsController {
         (r1, r2) => r1.distance - r2.distance
       );
     }
+
+    const CloseRangeData = ActiveSensors.arguments.scan;
   }
 }
