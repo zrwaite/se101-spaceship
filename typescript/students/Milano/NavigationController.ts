66840b2f65a9561c8593f3743067fc92e079249f
Darsh04-14 <darshpatelcr7@gmail.com>
2022-09-22 16:13:07 -0400

    test

--- typescript/students/Milano/NavigationController.ts
+++ typescript/students/Milano/NavigationController.ts
@@ -20,6 +20,6 @@ export default class YourNavigationController extends NavigationController {
 	navigationUpdate(getShipStatus: (key: keyof ShipStatus) => number, warp: () => Error|null, land: () => Error|null, getMapData: () => MapData) {
 		this.angle = getShipStatus('angle')
 		land()
-		//hello
+		//hello 😘
 	}
 }
