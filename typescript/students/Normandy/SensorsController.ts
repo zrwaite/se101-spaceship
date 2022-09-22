70ee1f7d89b1b918c5abdad44e57f2819632e7dd
Michael Xu <michael.xu1816@gmail.com>
2022-09-22 17:09:34 -0400

    Sensors detect denseTarget for defence team

--- typescript/students/Normandy/SensorsController.ts
+++ typescript/students/Normandy/SensorsController.ts
@@ -1,21 +1,59 @@
-import { withinPiRange, Vector2 } from '../helpers.js'
-import SensorsController from '../../src/subsystems/sensorsController.js'
-import YourDefenceController from './DefenseController.js'
-import YourNavigationController from './NavigationController.js'
-import YourPropulsionController from './PropulsionController.js'
-import { EMSReading, PassiveReading } from '../types.js'
+import { withinPiRange, Vector2 } from '../helpers.js';
+import SensorsController from '../../src/subsystems/sensorsController.js';
+import YourDefenceController from './DefenseController.js';
+import YourNavigationController from './NavigationController.js';
+import YourPropulsionController from './PropulsionController.js';
+import { EMSReading, PassiveReading } from '../types.js';
 export default class YourSensorsController extends SensorsController {
-	// To get other subsystem information, use the attributes below.
-	// @ts-ignore
-	defence: YourDefenceController // @ts-ignore
-	navigation: YourNavigationController // @ts-ignore
-	propulsion: YourPropulsionController
+  // To get other subsystem information, use the attributes below.
+  // @ts-ignore
+  defence: YourDefenceController; // @ts-ignore
+  navigation: YourNavigationController; // @ts-ignore
+  propulsion: YourPropulsionController;
 
-	//Add additional attributes here
-	target: PassiveReading | null = null
-	
-	sensorsUpdate(activeScan: (heading: number, arc: number, range: number) => EMSReading[] | Error, passiveScan: () => PassiveReading[] | Error) {
-		const scanResult = passiveScan()
-		if(!(scanResult instanceof Error )) this.target = scanResult[0]	
-	}	
+  //Add additional attributes here
+  // target: PassiveReading | null = null
+  target: PassiveReading | null = null;
+  defenseTarget: EMSReading | null = null;
+  // info: EMSReading | null = null
+
+  sensorsUpdate(
+    activeScan: (
+      heading: number,
+      arc: number,
+      range: number
+    ) => EMSReading[] | Error,
+    passiveScan: () => PassiveReading[] | Error
+  ) {
+    const passiveScanResult = passiveScan();
+    const activeScanResult = activeScan(
+      this.navigation.angle - Math.PI / 4,
+      Math.PI / 2,
+      100
+    );
+
+    if (!(passiveScanResult instanceof Error))
+      this.target = passiveScanResult[0];
+    // if(!(activeScanResult instanceof Error )) this.info = activeScanResult[0];
+
+    if (!(activeScanResult instanceof Error) && activeScanResult.length > 0) {
+      const badTargets = activeScanResult.filter(
+        (object) =>
+          object.closeRange &&
+          (object.closeRange.type === 'Asteroid' ||
+            object.closeRange.type === 'Meteor')
+      );
+
+      badTargets.sort(
+        (objectA, objectB) => objectA.distance - objectB.distance
+      );
+
+      this.defenseTarget = badTargets[0];
+    } else {
+      this.defenseTarget = null;
+    }
+
+    // this.defenseTarget;
+    // console.log(activeScanResult);
+  }
 }
