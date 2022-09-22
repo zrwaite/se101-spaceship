9260ee590edc7aa208969565bd48e019e4120c20
nandanjp <nandan.jp17@gmail.com>
2022-09-22 17:09:34 -0400

    Reverted back to original code

--- typescript/students/Milano/PropulsionController.ts
+++ typescript/students/Milano/PropulsionController.ts
@@ -4,13 +4,13 @@ import PropulsionController from '../../src/subsystems/propulsionController.js'
 import YourDefenceController from './DefenseController.js'
 import YourNavigationController from './NavigationController.js'
 import YourSensorsController from './SensorsController.js'
+
 export default class YourPropulsionController extends PropulsionController {
 	// To get other subsystem information, use the attributes below.
 	// @ts-ignore
 	defence: YourDefenceController // @ts-ignore
 	sensors: YourSensorsController // @ts-ignore
 	navigation: YourNavigationController
-	currDirection: Vector2 = new Vector2(0, 0) // @ts-ignore
 
 	//Add additional attributes here
 
@@ -19,23 +19,16 @@ export default class YourPropulsionController extends PropulsionController {
 		
 		const headingDiff = angleDiff(this.navigation.angle, this.sensors.target.heading);
 		const force = Math.min(Math.abs(100 * headingDiff), 100);
-
-		if(this.currDirection.equals(new Vector2(0, 0))) {	
-			this.currDirection.add(new Vector2(force, headingDiff));
-			setThruster("main", Math.abs(this.currDirection.angle()) < 0.1 ? 15000 : 100);
-		} else {
-			if(this.currDirection.angle() > headingDiff) {
-				this.currDirection.angleTo(new Vector2(force, headingDiff));
-			}
-		}
-
-		if (this.currDirection.angle() < 0) {
+		
+		if (headingDiff < 0) {
 			setThruster('clockwise', force)
 			setThruster('counterClockwise', force / 2)
 		} else {
 			setThruster('counterClockwise', force)
 			setThruster('clockwise', force / 2)
 		}
+		setThruster("main", Math.abs(headingDiff) < 0.1 ? 15000 : 100);
+
 
 	}
 	
