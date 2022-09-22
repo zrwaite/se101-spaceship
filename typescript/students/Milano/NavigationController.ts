dc8a5886d6385292581831bf0794cb232f6b2c4e
Dhyan Patel <djpatel@uwaterloo.ca>
2022-09-22 17:14:33 -0400

    Debugging array containing all navigation info

--- typescript/students/Milano/NavigationController.ts
+++ typescript/students/Milano/NavigationController.ts
@@ -16,13 +16,24 @@ export default class YourNavigationController extends NavigationController {
 	propulsion: YourPropulsionController
 
 	//Add additional attributes here
-	shipData: number[] = []
+	shipData1: number[] = []
 	
 
 	navigationUpdate(getShipStatus: (key: keyof ShipStatus) => number, warp: () => Error|null, land: () => Error|null, getMapData: () => MapData) {
 		
 		//hello
 		while (true){
+			this.shipData1[0] = getShipStatus('radius')
+			this.shipData1[1] = getShipStatus('angularVelocity')
+			this.shipData1[2] = getShipStatus('angle')
+			this.shipData1[3] = getShipStatus('positionX')
+			this.shipData1[4] = getShipStatus('positionY')
+			this.shipData1[5] = getShipStatus('linearVelocityX')
+			this.shipData1[6] = getShipStatus('linearVelocityY')
+			this.shipData1[7] = getShipStatus('thrusterPowerMain')
+			this.shipData1[8] = getShipStatus('thrusterPowerBow')
+			this.shipData1[9] = getShipStatus('thrusterPowerClockwise')
+			this.shipData1[10] = getShipStatus('thrusterPowerCounterClockwise')
 			land()
 			warp()
 			
