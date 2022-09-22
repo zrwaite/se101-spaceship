81384b33ceaf9e622ac5b402f7bbdd87f097888f
KathleenX7 <kxiong1210@gmail.com>
2022-09-22 16:21:24 -0400

    Defense edits

--- javascript/students/Bebop/DefenseController.js
+++ javascript/students/Bebop/DefenseController.js
@@ -4,9 +4,27 @@ export default class YourDefenceController extends DefenceController {
     defenceUpdate(aimTurret, getTubeCooldown, fireTorpedo) {
         if (!this.sensors.target)
             return;
-        if (this.sensors.activeScanData != null && this.sensors.activeScanData[0] != null && getTubeCooldown(0) == 0) {
-            aimTurret(this.sensors.activeScanData[0].angle);
-            fireTorpedo(0);
+        if (this.sensors.activeScanData != null) {
+            for (let i = 0; i < this.sensors.activeScanData.length; i++) {
+                if (this.sensors.activeScanData[i].closeRange) {
+                    if (getTubeCooldown(0) == 0) {
+                        aimTurret(this.sensors.activeScanData[0].angle);
+                        fireTorpedo(0);
+                    }
+                    else if (getTubeCooldown(1) == 0) {
+                        aimTurret(this.sensors.activeScanData[0].angle);
+                        fireTorpedo(1);
+                    }
+                    else if (getTubeCooldown(2) == 0) {
+                        aimTurret(this.sensors.activeScanData[0].angle);
+                        fireTorpedo(2);
+                    }
+                    else if (getTubeCooldown(3) == 0) {
+                        aimTurret(this.sensors.activeScanData[0].angle);
+                        fireTorpedo(3);
+                    }
+                }
+            }
         }
     }
 }
--- typescript/students/Bebop/DefenseController.ts
+++ typescript/students/Bebop/DefenseController.ts
@@ -12,10 +12,24 @@ export default class YourDefenceController extends DefenceController {
 	//Add additional attributes here
 	defenceUpdate(aimTurret: (angle: number) => void, getTubeCooldown: (i: number) => number | Error, fireTorpedo: (i: number) => Error | null) {
 		if (!this.sensors.target) return
-		if(this.sensors.activeScanData != null && this.sensors.activeScanData[0] != null && getTubeCooldown(0) == 0){
-			aimTurret(this.sensors.activeScanData[0].angle)
-			fireTorpedo(0)
+		if(this.sensors.activeScanData != null){
+			for(let i = 0; i < this.sensors.activeScanData.length; i++){
+				if(this.sensors.activeScanData[i].closeRange){
+					if(getTubeCooldown(0) == 0){
+						aimTurret(this.sensors.activeScanData[0].angle)
+						fireTorpedo(0)
+					}else if(getTubeCooldown(1) == 0){
+						aimTurret(this.sensors.activeScanData[0].angle)
+						fireTorpedo(1)
+					}else if(getTubeCooldown(2) == 0){
+						aimTurret(this.sensors.activeScanData[0].angle)
+						fireTorpedo(2)
+					}else if(getTubeCooldown(3) == 0){
+						aimTurret(this.sensors.activeScanData[0].angle)
+						fireTorpedo(3)
+					}
+				}
+			}
 		}
-		
 	}
 }
