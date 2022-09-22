7887a96abd02c008cf014ff0455bef4ed82cc02e
Peter Wang <petemango@v1040-wn-rt-a-43-2.campus-dynamic.uwaterloo.ca>
2022-09-22 18:03:33 -0400

    unshift instead of push

--- javascript/students/Normandy/SensorsController.js
+++ javascript/students/Normandy/SensorsController.js
@@ -20,7 +20,7 @@ export default class YourSensorsController extends SensorsController {
             const scanResult = passiveScan();
             if (!(scanResult instanceof Error)) {
                 this.target = scanResult[0];
-                this.passiveScans.push(scanResult);
+                this.passiveScans.unshift(scanResult);
                 console.log(this.passiveScans);
             }
             this.passiveCooldown = 100;
@@ -31,7 +31,7 @@ export default class YourSensorsController extends SensorsController {
         }
         const activeScanResult = activeScan(0, Math.PI, 1000);
         if (!(activeScanResult instanceof Error) && (activeScanResult === null || activeScanResult === void 0 ? void 0 : activeScanResult.length) > 0) {
-            this.activeScans.push(activeScanResult);
+            this.activeScans.unshift(activeScanResult);
             const badTargets = activeScanResult.filter((object) => object.closeRange && (object.closeRange.type === 'Asteroid' || object.closeRange.type === 'Meteor'));
             badTargets.sort((objectA, objectB) => objectA.distance - objectB.distance);
             this.defenseTarget = badTargets[0];
