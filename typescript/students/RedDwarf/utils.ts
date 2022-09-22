9a89929ab54f2d4d5d217180c41765704b74015c
ProximityTrigger <proximitytrigger@gmail.com>
2022-09-22 13:51:48 -0400

    utils.ts: create getFullShipStatus function.

--- typescript/students/RedDwarf/utils.ts
+++ typescript/students/RedDwarf/utils.ts
@@ -1,3 +1,23 @@
-export function getShip() {
+import ColonyShip from "../../src/ship/colonyShip";
+import { ShipStatus } from "../types";
+
+export function getShip() : ColonyShip {
     return (<any>window).game.ships[0];
+}
+
+export function getFullShipStatus() : ShipStatus {
+    const ship = getShip();
+    return {
+        positionX: ship.pos.x,
+        positionY: ship.pos.y,
+        radius: ship.radius,
+        linearVelocityX: ship.speed.x,
+        linearVelocityY: ship.speed.y,
+        angularVelocity: ship.aSpeed,
+        angle: ship.angle,
+        thrusterPowerMain: ship.thrusterController.thrusterPower.main,
+        thrusterPowerBow: ship.thrusterController.thrusterPower.bow,
+        thrusterPowerClockwise: ship.thrusterController.thrusterPower.clockwise,
+        thrusterPowerCounterClockwise: ship.thrusterController.thrusterPower.counterClockwise,
+    };
 }
