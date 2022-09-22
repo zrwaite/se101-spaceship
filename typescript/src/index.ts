600739cc28c176d85a8e02bcdeba48f90d8113be
ProximityTrigger <proximitytrigger@gmail.com>
2022-09-22 17:07:28 -0400

    !-

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -373,7 +373,7 @@ let DOM: any = {
 	},
 	doneLoad: () => {
 		contexts['background'].drawImage(images['background'], 0, 0, windowSize.x * unit, windowSize.y * unit);
-		(<any>window).game = game = new Game(windowSize.x, windowSize.y, images, contexts, DOM.landSuccessful);
+		game = new Game(windowSize.x, windowSize.y, images, contexts, DOM.landSuccessful);
 		game.unit = unit
 		DOM.loaded = true
 		DOM.initialize()
